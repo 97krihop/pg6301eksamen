7f93e85b4b9ea6b0ea82ef9a679a884ce58a05d9
Kristoffer Larsen Hopland <24739853+97krihop@users.noreply.github.com>
2021-04-27 14:28:20 +0200

    added more user Test

--- __tests__/server/db/users-test.ts
+++ __tests__/server/db/users-test.ts
@@ -2,6 +2,7 @@ import {
   clearAllUsers,
   createUser,
   getUser,
+  verifyUser,
 } from "../../../src/server/db/users";
 
 describe("users DB tests", () => {
@@ -35,5 +36,19 @@ describe("users DB tests", () => {
       createUser(user.email, user.password, user.firstname, user.lastname)
     ).toBe(false);
     expect(getUser(user.email)).toEqual(user);
+  });
+  it("should verify a user", async () => {
+    const user = {
+      firstname: "bob",
+      lastname: "hansen",
+      email: "bob@bob.com",
+      password: "password",
+    };
+    expect(
+      createUser(user.email, user.password, user.firstname, user.lastname)
+    ).toBe(true);
+    expect(verifyUser(user.email, user.password)).toBe(true);
+    expect(verifyUser("something else", user.password)).toBe(false);
+    expect(verifyUser(user.email, "something else")).toBe(false);
   });
 });
