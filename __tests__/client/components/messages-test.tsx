02e000dae64935d97d703a3a36eb3fe8059aae1f
Kristoffer Larsen Hopland <24739853+97krihop@users.noreply.github.com>
2021-04-27 03:28:04 +0200

    added tests for Messages component

---
+++ __tests__/client/components/messages-test.tsx
@@ -0,0 +1,65 @@
+import { act, render, screen } from "@testing-library/react";
+import React from "react";
+import { Messages } from "../../../src/client/components/messages";
+import { useLoading } from "../../../src/client/lib/useLoading";
+import userEvent from "@testing-library/user-event";
+import { useSubmit } from "../../../src/client/lib/useSubmit";
+
+jest.mock("../../../src/client/lib/useLoading");
+jest.mock("../../../src/client/lib/useSubmit");
+let mockFn1 = jest.fn();
+let mockFn2 = jest.fn();
+beforeEach(() => {
+  mockFn1.mockReset();
+  mockFn2.mockReset();
+  // @ts-ignore
+  useLoading.mockReturnValue({
+    error: null,
+    loading: false,
+    data: [{ email: "bob@example.com", message: "test" }],
+    reload: mockFn1,
+  });
+  // @ts-ignore
+  useSubmit.mockReturnValue({
+    handleSubmit: (e: Event) => {
+      e.preventDefault();
+      mockFn2();
+    },
+    submitting: false,
+    error: null,
+  });
+});
+describe("test message component", () => {
+  it("should render messages", async () => {
+    act(() => {
+      render(<Messages />);
+    });
+    const mes = screen.getByText("message:");
+    const res = screen.getByText("send to:");
+    screen.getByText("(email split on space)");
+    const sub = screen.getByText("submit a socket message");
+    const rel = screen.getByText("reload");
+    await act(async () => {
+      await userEvent.type(mes, "test");
+      await userEvent.type(res, "test");
+      await userEvent.click(rel);
+      await userEvent.click(sub);
+    });
+    await screen.findAllByText("bob@example.com : test");
+    expect(mockFn1).toHaveBeenCalledTimes(1);
+    expect(mockFn2).toHaveBeenCalledTimes(1);
+  });
+  it("should render loading", () => {
+    // @ts-ignore
+    useLoading.mockReturnValue({
+      error: null,
+      loading: true,
+      data: [{ email: "bob@example.com", message: "test" }],
+      reload: mockFn1,
+    });
+    act(() => {
+      render(<Messages />);
+    });
+    screen.getByText("Loading...");
+  });
+});
